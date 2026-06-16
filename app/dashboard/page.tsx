e8913b1ac714dac0c8c4df8f28384fc836cc784c
alaninnovates <alanlichen1@gmail.com>
2026-06-16 20:58:12 +0800

    Start work on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,39 +1,53 @@
-import {redirect} from "next/navigation";
+// import {redirect} from "next/navigation";
+// import {createClient} from "@/lib/supabase/server";
+import {Container, Box, Flex, Card, Text, CardSection, Button, Image} from "@mantine/core";
 
-import {createClient} from "@/lib/supabase/server";
-import {Suspense} from "react";
-import {LogoutButton} from "@/components/logout-button";
+// async function UserDetails() {
+//     const supabase = await createClient();
+//     const {data, error} = await supabase.auth.getClaims();
+//
+//     if (error || !data?.claims) {
+//         redirect("/auth/login");
+//     }
+//
+//     return JSON.stringify(data.claims, null, 2);
+// }
 
-async function UserDetails() {
-    const supabase = await createClient();
-    const {data, error} = await supabase.auth.getClaims();
-
-    if (error || !data?.claims) {
-        redirect("/auth/login");
-    }
-
-    return JSON.stringify(data.claims, null, 2);
-}
-
-export default function ProtectedPage() {
+export default function DashboardPage() {
     return (
-        <div className="protected-page">
-            <LogoutButton/>
-            <div>
-                <div className="notice">
-                    <span aria-hidden="true">i</span>
-                    This is a protected page that you can only see as an authenticated
-                    user
-                </div>
-            </div>
-            <section className="content-section">
-                <h2>Your user details</h2>
-                <pre className="user-details">
-          <Suspense>
-            <UserDetails/>
-          </Suspense>
-        </pre>
-            </section>
-        </div>
+        <Container w="100%">
+            <h1>My Ensembles</h1>
+            <Flex
+                gap="md"
+                justify="flex-start"
+                align="flex-start"
+                direction="row"
+                wrap="wrap"
+            >
+                <Card padding="sm" withBorder orientation="horizontal">
+                    <CardSection px="sm">
+                        <Image
+                            bg="blue"
+                            radius="md"
+                            p="lg"
+                            w="80%"
+                            src={"https://files.slack.com/files-pri/T0266FRGM-F0BA3RJDS31/image.png?pub_secret=5d010e5132"}/>
+                    </CardSection>
+
+                    <CardSection px="sm">
+                        <Text fz="xl">LAHS MBCG</Text>
+                        <Box mt="xs">
+                            <Text>6</Text>
+                            <Text fz="xs" c="dimmed">
+                                Members
+                            </Text>
+                        </Box>
+                        <Button fullWidth mt="md">
+                            Ensemble Shows
+                        </Button>
+                    </CardSection>
+                </Card>
+            </Flex>
+        </Container>
     );
 }

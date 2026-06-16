f7bfd49a04b6a5eca68bb228449ca4a2886e8083
alaninnovates <alanlichen1@gmail.com>
2026-06-16 21:38:56 +0800

    Display actual ensemble data in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,68 @@
+"use client"
+import {useEffect, useState} from "react";
+import {redirect} from "next/navigation";
 import {Container, Box, Flex, Card, Text, CardSection, Button, Image} from "@mantine/core";
+import {createClient} from "@/lib/supabase/client";
+import Link from "next/link";
+
+interface Ensemble {
+    id: number;
+    name: string;
+    icon: string;
+    role: 'awaiting_approval' | 'member' | 'admin';
+    approved_at: string;
+    member_count: number;
+}
 
 export default function DashboardPage() {
+    const [ensembles, setEnsembles] = useState<undefined | Ensemble[]>();
+
+    useEffect(() => {
+        const supabase = createClient();
+
+        (async () => {
+            const {data: claims, error: claimsError} = await supabase.auth.getClaims();
+            if (claimsError || !claims?.claims) {
+                redirect("/auth/login");
+            }
+            const {data: ensembles, error: ensembleError} = await supabase
+                .from('ensemble_memberships')
+                .select('ensembles(id,name,icon), role, approved_at')
+                .neq('role', 'awaiting_approval')
+                .order('approved_at')
+                .eq('user_id', claims.claims.sub);
+
+            if (ensembleError || !ensembles.length) {
+                console.error("err ensemble:", ensembleError);
+                return;
+            }
+
+            const ensemblesData: Ensemble[] = [];
+
+            for (const e of ensembles) {
+                const {count: memberCount, error: memberCountError} = await supabase
+                    .from('ensemble_memberships')
+                    .select('user_id', {count: 'exact', head: true})
+                    .eq('ensemble_id', e.ensembles.id);
+
+                if (!memberCount || memberCountError) {
+                    console.error("err mb cnt:", memberCountError);
+                    return;
+                }
+
+                ensemblesData.push({
+                    id: e.ensembles.id,
+                    name: e.ensembles.name,
+                    icon: e.ensembles.icon,
+                    role: e.role,
+                    approved_at: e.approved_at,
+                    member_count: memberCount
+                });
+            }
+            setEnsembles(ensemblesData);
+        })()
+    }, []);
+
     return (
         <Container w="100%">
             <h1>My Ensembles</h1>
@@ -11,29 +73,34 @@ export default function DashboardPage() {
                 direction="row"
                 wrap="wrap"
             >
-                <Card padding="sm" withBorder orientation="horizontal">
-                    <CardSection px="sm">
-                        <Image
-                            bg="blue"
-                            radius="md"
-                            p="lg"
-                            w="80%"
-                            src={"https://files.slack.com/files-pri/T0266FRGM-F0BA3RJDS31/image.png?pub_secret=5d010e5132"}/>
-                    </CardSection>
+                {/*{!ensembles && (*/}
+                {/*    <Te*/}
+                {/*)}*/}
+                {ensembles && ensembles.map(e => (
+                    <Card padding="sm" withBorder orientation="horizontal" key={e.id}>
+                        <CardSection p="sm">
+                            <Image
+                                bg="blue"
+                                radius="md"
+                                p="lg"
+                                w="80%"
+                                src={e.icon}/>
+                        </CardSection>
 
-                    <CardSection px="sm">
-                        <Text fz="xl">LAHS MBCG</Text>
-                        <Box mt="xs">
-                            <Text>6</Text>
-                            <Text fz="xs" c="dimmed">
-                                Members
-                            </Text>
-                        </Box>
-                        <Button fullWidth mt="md">
-                            Ensemble Shows
-                        </Button>
-                    </CardSection>
-                </Card>
+                        <CardSection p="sm">
+                            <Text fz="xl">{e.name}</Text>
+                            <Box mt="xs">
+                                <Text>{e.member_count}</Text>
+                                <Text fz="xs" c="dimmed">
+                                    Members
+                                </Text>
+                            </Box>
+                            <Button fullWidth mt="md" component={Link} href={`/dashboard/ensemble/${e.id}`}>
+                                Ensemble Shows
+                            </Button>
+                        </CardSection>
+                    </Card>
+                ))}
             </Flex>
         </Container>
     );
